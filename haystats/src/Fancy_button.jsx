4f3cbdcbc7943fd8caaea092aa9b13c0edc631c9
Melda Kiziltan <77182114+MeldaKiziltan@users.noreply.github.com>
2021-01-16 18:34:10 -0500

    Fancier button

--- haystats/src/Fancy_button.jsx
+++ haystats/src/Fancy_button.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+import FindInPageIcon from '@material-ui/icons/FindInPage';
 
 // We can inject some CSS into the DOM.
 const styles = {
@@ -21,7 +22,7 @@ function ClassNames(props) {
   const { classes, children, className, ...other } = props;
 
   return (
-    <Button className={clsx(classes.root, className)} {...other}>
+    <Button className={clsx(classes.root, className)} {...other} startIcon={<FindInPageIcon />}>
       {children || 'Click to find your needle in a haystack!'}
     </Button>
   );
