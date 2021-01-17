adfc549b1e9985c3604384fb948e39e47f540464
Patrick Kim <s524kim@uwaterloo.ca>
2021-01-16 23:56:33 -0800

    final x4

--- haystats/src/watsonIntegration.js
+++ haystats/src/watsonIntegration.js
@@ -71,7 +71,9 @@ function watson(URL, setSummary){
     }
 
     //add sentences
-    let sentences = article_text.split(". ");
+
+    let sentences = str.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+    //let sentences = article_text.split(". ");
     //let sentences = article_text.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
     let statistics = [];
 
